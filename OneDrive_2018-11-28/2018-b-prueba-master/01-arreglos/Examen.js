6cbb954ac257ffcc346b7075f07a5e5d85d49bcc
Capamed <39675448+Capamed@users.noreply.github.com>
2018-11-28 11:00:23 -0500

    examen cambios todo

--- OneDrive_2018-11-28/2018-b-prueba-master/01-arreglos/Examen.js
+++ OneDrive_2018-11-28/2018-b-prueba-master/01-arreglos/Examen.js
@@ -1,4 +1,7 @@
+const inquirer = require ('inquirer');
 const fs = require('fs');
+
+///////////////////////////////////////////////////////////////7EJERCICIO 1
 function buscarGENERO() {
     return new Promise(
         (resolve, reject) => {
@@ -38,6 +41,9 @@ buscarGENERO()
                     arregloFemale.push(personas)
                         console.log('fFEMALE',arregloFemale)
                     }
+
+////////////////queria hacer más bonita la 'prpesentacionm pero ya no habia tiempo para anailizarle más pero si deplega lo requerio
+                    //solo que desplega cada que el map ////////////////////////////////////////////////////////EJERCICIO 5
                 }
 
             }
@@ -53,7 +59,7 @@ buscarGENERO()
     )
 
 
-///////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////EJERCICIO 2
 function buscarOJOS() {
     return new Promise(
         (resolve, reject) => {
@@ -88,7 +94,7 @@ buscarOJOS()
             console.log('Algo malo paso',resultadoError);
         }
     )
-//////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////// EJERCICIO 3
 function colorPIEL() {
     return new Promise(
         (resolve, reject) => {
@@ -123,7 +129,7 @@ colorPIEL()
             console.log('Algo malo paso',resultadoError);
         }
     )
-/////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////// EJERCICIO 4
 function colorPIEL() {
     return new Promise(
         (resolve, reject) => {
@@ -159,12 +165,180 @@ colorPIEL()
         }
     )
 
-/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////    EJERECICIO 6
 const arregloRespuesta =[
-    {
-        a:true
-    },
-    {
-        b:false
-    },
+
 ];
+
+function buscarABECEDARIO() {
+    return new Promise(
+        (resolve, reject) => {
+            fs.readFile('people.json', 'utf-8',
+                (err, contenido) => {
+                    if (err) {
+                        reject({mensaje: 'Error leyendo'});
+                    } else {
+                        const bdd = JSON.parse(contenido);
+                        const respuestaFind = bdd.persona
+                            .map(
+                                (people)=>{
+                                    return  people.name;
+                                }
+                            )
+
+                        resolve(respuestaFind);
+                    }
+                });
+        }
+    );
+}
+
+
+
+buscarABECEDARIO()
+    .then(
+        (contenidoArchivo)=>{
+            console.log(contenidoArchivo)
+        contenidoArchivo.map(
+            (usuarios)=>{
+               if(usuarios === 'a'|usuarios === 'b'|usuarios === 'c'|usuarios === 'd'|usuarios === 'e'){
+                   arregloRespuesta.push()
+               }
+            }
+         )
+        }
+    )
+    .catch(
+        (resultadoError)=>{
+            console.log('Algo malo paso',resultadoError);
+        }
+    )
+
+
+//////////////////////////////////////////////////////////EJERCICIO 11///////////////////////////
+
+const opcionesMenu ={
+    type: 'list',
+    name: 'opcionMenu',
+    message: 'Escoge la opción qué desees: ',
+    choices: [
+        'Crear',
+    ]
+};
+
+const preguntasAlUsuario = [
+    { type: 'input', name: 'nombre', message: 'Ingrese el nombre del personaje: '},
+    { type: 'input', name: 'specie', message: 'Ingrese la specie: '},
+
+];
+
+main();
+async function main() {
+    try{
+        await inicilizarBase();
+        const  respuestaOpcionMenu = await inquirer.prompt(opcionesMenu);
+        console.log('Respuesta.opcionMenu: ',respuestaOpcionMenu.opcionMenu);
+        switch (respuestaOpcionMenu.opcionMenu) {
+            case 'Crear':
+                console.log('Pedir Datos');
+                const respuestaPreguntasAlUsuario = await inquirer.prompt(preguntasAlUsuario);
+                console.log('respuestas al DeTipoUsuario',respuestaPreguntasAlUsuario);
+                const respuestaAñadirUsuario = await añadirDatosALaBase(respuestaPreguntasAlUsuario);
+                main();
+                break;
+        }
+
+    } catch (e) {
+        console.log('HUBO UN ERROR')
+    }
+}
+function inicilizarBase(){
+    return new Promise(
+        (resolve,reject)=>{
+            fs.readFile('people1.json','utf-8',
+                (error,contenidoLeido)=>{
+                    if(error){
+                        fs.writeFile('people.json','{"usuarios":[],"juegos":[]}',
+                            (error)=>{
+                                if(error){
+                                    reject(
+                                        {mensaje:'HUBO UN ERROR AL CREAR EL ARCHIVO'}
+                                    )
+                                }else{
+                                    resolve(
+                                        {mensaje:'ok'}
+                                    );
+                                }
+                            }
+                        );
+                    }else {
+                        resolve(
+                            {mensaje:'ok'}
+                        );
+                    }
+                }
+            );
+        }
+    );
+};
+
+function añadirDatosALaBase(usuarioIngresarEnLaBase){
+    return new Promise(
+        (resolve,reject)=>{
+            fs.readFile('people1.json','utf-8',
+                (error,contenidoLeido)=>{
+                    if(error){
+                        reject(
+                            {mensaje:'ERROR AL LEER EL ARCHIVO'}
+                        );
+                    }else {
+                        //jason parse-->para transformar de STRING a objeto JSON
+                        const bddJSONPARSE = JSON.parse(contenidoLeido);
+                        bddJSONPARSE.persona.push(usuarioIngresarEnLaBase);
+                        fs.writeFile('people1.json',JSON.stringify(bddJSONPARSE),
+                            (error)=>{
+                                if(error){
+                                    reject(
+                                        {mensaje:'ERROR AL ESCRIBIR EL ARCHIVO'}
+                                    );
+                                }else{
+                                    resolve(
+                                        {mensaje:'USUARIO AÑADIDO EN LA BASE'}
+                                    );
+                                }
+                            }
+
+                        );
+                    }
+                }
+            );
+        }
+    );
+};
+
+
+
+//////////////////////////////SUMARTORIA
+
+function sumatoriaHL() {
+    return new Promise(
+        (resolve, reject) => {
+            fs.readFile('people.json', 'utf-8',
+                (err, contenido) => {
+                    if (err) {
+                        reject({mensaje: 'Error leyendo'});
+                    } else {
+                        const bdd = JSON.parse(contenido);
+                        const respuestaFind = bdd.persona
+                            .map(
+                                (people)=>{
+                                    return  people.massTotal;
+                                }
+                            )
+
+                        resolve(respuestaFind);
+                    }
+                });
+        }
+    );
+}
